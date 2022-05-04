bfab6ea27b91a6dd78b6b42e875b47680c939eb7
tarek5171 <104913535+tarek5171@users.noreply.github.com>
2022-05-04 22:02:10 +0300

    Add files via upload

--- clients_viewcars.php
+++ clients_viewcars.php
@@ -61,6 +61,7 @@ if(isset($_POST['client_id'])){
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto py-4 py-lg-0">
+                     <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="clients.php">Back</a></li>
 
                     </ul>
                 </div>
